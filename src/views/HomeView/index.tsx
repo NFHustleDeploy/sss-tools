6eda1733eee2201571443122829be5f81b718733
NFHustle <nfhustle@outlook.com>
2022-09-24 03:16:38 -0400

    Update index.tsx

--- src/views/HomeView/index.tsx
+++ src/views/HomeView/index.tsx
@@ -51,8 +51,8 @@ export const HomeView: FC = ({ }) => {
                 <p className="mb-2">
                 Ape Dao Society
                 <br />
-                First Tombstoned DAO Based on continuing the vibes of community Idealism that the Tombstoned high society has worked to built
-The First agenda the DAO worked towards was minting a y00t together, Through this success the ape Dao discord was born and our numbers grew!
+                First Tombstoned DAO based on continuing the vibes of community idealism that the Tombstoned high society has worked to build.
+The First agenda the DAO worked towards was minting a y00t together, through this success the APE DAO discord was born and our numbers grew!
 A total of 282 apes roam the Graveyard, Will you join? 
                 <br />
                 <em>Twitter:</em><a href="https://twitter.com/TombstonedApes">@TombstonedApes</a>
